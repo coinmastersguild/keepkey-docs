6b56a8b5bed360b3076fc6679e2ba601394032c5
bithighlander <bithighlander@gmail.com>
2024-04-09 23:15:37 -0500

    begin docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  images: {
+    domains: ['miro.medium.com','www.keepkey.com'],
+  },
+  // You might have other configurations here
+});
